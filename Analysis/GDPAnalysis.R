a47390a1ffb17ba2b6558c61afd3540ed4bec0da
jcatam1999 <jcatam99@gmail.com>
2016-11-02 21:22:10 -0500

    Commit final changes

--- Analysis/GDPAnalysis.R
+++ Analysis/GDPAnalysis.R
@@ -9,32 +9,26 @@ gdpedu = read.csv(GdpEduFile)
 
 
 #print 13th country
+print('The 13th Country in the Order Data is:')
 print(gdpedu$Long.Name[13])
 
 ##Question 3 average ranking of High income OECD and nonOECD
+print('The average ranking for High Income: nonOECD is:')
 print(mean(gdpedu$Ranking[gdpedu$Income.Group =='High income: nonOECD']))
+print('The average ranking for High Income is:')
 print(mean(gdpedu$Ranking[gdpedu$Income.Group =='High income: OECD']))
 
-##plot data
-##gdpplot = ggplot(gdpedu, aes(gdpedu$Income.Group, gdpedu$GDP))+geom_point(color='firebrick')
+##question 4:
+print('The number of countries classified as Lower Income but are amoung the 38 highest GDP Nations:')
+print(nrow(gdpedu[(gdpedu$Quantile==1) & (gdpedu$Income.Group =='Lower middle income'),]))
 
-
+#plot data
 gdpplot = ggplot(gdpedu, aes(gdpedu$Region, gdpedu$GDP, color=factor(gdpedu$Income.Group)))+geom_point()
 gdpplot = gdpplot+theme(axis.text.x=element_text(angle = 35, size=10, vjust=.25))
 gdpplot = gdpplot+ggtitle('GDP By Region')
-gdpplot = gdpplot+labs(x='Income Group', y='GDP (Millions')
+gdpplot = gdpplot+labs(x='Region', y='GDP (Millions)')
 gdpplot = gdpplot+theme(legend.title = element_blank())
 print(gdpplot)
-
-##Break down into quantile groups
-gdpedu$Quantile = ntile(gdpedu$Ranking, 5)
-
-##question 4:
-print(nrow(gdpedu[(gdpedu$Quantile==1) & (gdpedu$Income.Group =='Lower middle income'),]))
-
-
-
-
 
 
 
--- Analysis/MergeFormat.R
+++ Analysis/MergeFormat.R
@@ -19,6 +19,7 @@ gdpedu = merge(gdp, edu, by='CountryCode', sort = TRUE)
 
 
 ##Question 1 How many ID's Matched
+print('Number of Matching country codes')
 print(nrow(gdpedu))
 
 ##format GDP 
@@ -29,9 +30,13 @@ gdpedu = gdpedu[order(gdpedu$GDP),]
 
 ##remove any non-country country codes. only interested is countries, not regions
 ##count first
+print('Number of rows removed during clean up')
 print(sum(is.na(gdpedu$Ranking)))
 ##now remove them
 gdpedu = subset(gdpedu, gdpedu$Ranking !='NA')
+
+##Break down into quantile groups
+gdpedu$Quantile = ntile(gdpedu$Ranking, 5)
 
 ##write out final data file
 write.csv(gdpedu, file=GdpEduFile)
